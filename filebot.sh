187515e8128f3d2398c262a713757b3ddfb2de27
terrac <terracnosaur@gmail.com>
2018-09-21 04:25:34 +0000

    updates

--- filebot.sh
+++ filebot.sh
@@ -3,20 +3,20 @@
 SERVICE="filebot"
 IMAGE="rednoah/filebot"
 VERSION="node"
-LOCALDIR="/data01"
+DATADIR="/data01"
 
 docker stop ${SERVICE}
 docker rm ${SERVICE}
 
 sudo docker run -d \
-  -p 5452:5452 \
-  --restart=always \
+  -p 8906:5452 \
+  -e FILEBOT_CMD_UID=1001 -e FILEBOT_CMD_GID=1001 \
   --name=${SERVICE} \
   --hostname=${HOSTNAME} \
-  -v /data01/services/${SERVICE}:/config \
-  -v /M2scratch/autorippr/:/input01 \
-  -v /data01:/input02 \
-  -v /data01/complete/:/dest \
+  -v ${DATADIR}/services/${SERVICE}:/config \
+  -v /M2scratch/:/M2Scratch \
+  -v ${DATADIR}/:/volume1/download/ \
+  -v ${DATADIR}/complete:/dest \
   ${IMAGE}:${VERSION}
 
 docker logs ${SERVICE}
